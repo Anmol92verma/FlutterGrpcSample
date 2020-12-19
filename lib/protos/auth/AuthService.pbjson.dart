935321762498b9704db7255740394fe9ada81299
Anmol Verma <anmol.verma4@gmail.com>
2020-12-19 21:34:23 +0530

    updated verification with twilio

--- lib/protos/auth/AuthService.pbjson.dart
+++ lib/protos/auth/AuthService.pbjson.dart
@@ -1,23 +1,30 @@
 ///
 //  Generated code. Do not modify.
-//  source: lib/protos/AuthService.proto
+//  source: AuthService.proto
 //
 // @dart = 2.3
 // ignore_for_file: annotate_overrides,camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type,unnecessary_this,prefer_final_fields
 
-const SignupRequest$json = const {
-  '1': 'SignupRequest',
+const AuthRequest$json = const {
+  '1': 'AuthRequest',
   '2': const [
-    const {'1': 'fullName', '3': 1, '4': 1, '5': 9, '10': 'fullName'},
-    const {'1': 'userName', '3': 2, '4': 1, '5': 9, '10': 'userName'},
+    const {'1': 'phoneNumber', '3': 1, '4': 1, '5': 9, '10': 'phoneNumber'},
   ],
 };
 
-const SignupResponse$json = const {
-  '1': 'SignupResponse',
+const AuthResponse$json = const {
+  '1': 'AuthResponse',
   '2': const [
     const {'1': 'code', '3': 1, '4': 1, '5': 5, '10': 'code'},
     const {'1': 'message', '3': 2, '4': 1, '5': 9, '10': 'message'},
+  ],
+};
+
+const AuthVerify$json = const {
+  '1': 'AuthVerify',
+  '2': const [
+    const {'1': 'phoneNumber', '3': 1, '4': 1, '5': 9, '10': 'phoneNumber'},
+    const {'1': 'code', '3': 2, '4': 1, '5': 9, '10': 'code'},
   ],
 };
 
